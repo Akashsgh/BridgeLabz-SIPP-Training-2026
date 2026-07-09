21c513084c372e04ea1aa20bfa74aa24ccb6c6a3
Akash <akash@example.com>
2026-07-09 23:47:42 +0530

    Add Binary Search programs

---
+++ Collection_DSA/Binary_Search/SearchMatrix.java
@@ -0,0 +1,40 @@
+package Collection_DSA.Binary_Search;
+
+class SearchMatrix {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int value = matrix[mid / cols][mid % cols];
+
+            if (value == target)
+                return true;
+            else if (value < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1,3,5},
+                {7,9,11},
+                {13,15,17}
+        };
+
+        System.out.println(searchMatrix(matrix,9));
+    }
+}
